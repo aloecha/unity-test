0d0502d76763f5a7e787c56fce526c601df6a2da
aloecha <162111018+aloecha@users.noreply.github.com>
2024-04-22 15:56:49 +0100

    character controller

---
+++ test github/Assets/character controller.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class charactercontroller : MonoBehaviour
+{
+    Animator Low Poly Human Animator;
+    pivate void Awake()
+    {
+        Low Poly Human Animator = Getcomponent
+    }
+}
